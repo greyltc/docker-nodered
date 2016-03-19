d905b807d21905077c8b4c2c139b848cad997a5b
Grey Christoforo <grey@christoforo.net>
2016-03-19 22:08:59 +0000

    add come more nifty nodes

--- setup-nodered.sh
+++ setup-nodered.sh
@@ -4,4 +4,7 @@ pacman -S --needed --noconfirm --noprogress npm base-devel python2
 npm install -g --unsafe-perm node-pre-gyp
 npm install -g --unsafe-perm node-red
 npm install -g --unsafe-perm node-red-contrib-scx-ibmiotapp
-
+npm install -g --unsafe-perm node-red-node-emoncms
+#npm install -g --unsafe-perm node-red-node-leveldb
+npm install -g --unsafe-perm node-red-node-mysql
+npm install -g --unsafe-perm node-red-node-sqlite
