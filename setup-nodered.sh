8863a2425408ce66816a78a4ec1af8fbac17b08a
Grey Christoforo <grey@christoforo.net>
2016-03-19 22:12:38 +0000

    Update setup-nodered.sh

--- setup-nodered.sh
+++ setup-nodered.sh
@@ -3,8 +3,10 @@
 pacman -S --needed --noconfirm --noprogress npm base-devel python2
 npm install -g --unsafe-perm node-pre-gyp
 npm install -g --unsafe-perm node-red
+# a nodes list is here: https://github.com/node-red/node-red-nodes
 npm install -g --unsafe-perm node-red-contrib-scx-ibmiotapp
 npm install -g --unsafe-perm node-red-node-emoncms
 #npm install -g --unsafe-perm node-red-node-leveldb
 npm install -g --unsafe-perm node-red-node-mysql
 npm install -g --unsafe-perm node-red-node-sqlite
+
